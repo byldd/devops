1f12267eae175c8427ab1f3d9d4533fd5552b6c3
Khalid Khan <khalidkhan@Khalids-MacBook-Air.local>
2025-05-18 23:35:39 +0530

    fix: fix issues

--- services/ecr-creds-manager.sh
+++ services/ecr-creds-manager.sh
@@ -4,9 +4,7 @@ amazon_ecr_credential_helper_installer() {
       command -v docker-credential-ecr-login
       ;;
     install)
-      curl -fsSL https://d1vvhvl2y92vvt.cloudfront.net/amazon-ecr-credential-helper.gpg | sudo gpg --dearmor -o /usr/share/keyrings/amazon.gpg
-      echo "deb [signed-by=/usr/share/keyrings/amazon.gpg] https://d1vvhvl2y92vvt.cloudfront.net stable main" | sudo tee /etc/apt/sources.list.d/amazon-ecr-helper.list
-      sudo apt-get update
+      sudo apt-get update -y
       sudo apt-get install -y amazon-ecr-credential-helper
       ;;
   esac
