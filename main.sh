c5d02311dd65b0d9ff74552bee2841bc506f01d4
Khalid Khan <khalidkhan@Khalids-MacBook-Air.local>
2025-05-19 04:21:55 +0530

    fix: fix issues

--- main.sh
+++ main.sh
@@ -16,3 +16,13 @@ install_with_progress "Docker" docker_installer
 install_with_progress "Amazon-ECR-Credential-Helper" amazon_ecr_credential_helper_installer
 install_with_progress "Caddy" caddy_installer
 install_with_progress "Infisical" infisical_installer
+
+# Configure Watchtower
+# Mount aws ecr credential helper volume to host machine so that it can be used inside watchtower for ecr creds helper installation
+# ref: https://containrrr.dev/watchtower/private-registries/#credential_helpers
+echo "✨ Step : Configuring watchtower"
+docker run  -d --rm --name aws-cred-helper --volume helper:/go/bin tanishbyldd/aws-ecr-dock-cred-helper
+echo "✔️  Watchtower configuration completed"
+echo "-----------------------"
+echo
+echo " 🚀 All services are configured successfully"
