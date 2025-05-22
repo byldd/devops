c9ae1494478f8527635632fa4a453bfc8b9bc707
Khalid Khan <khalidkhan@Khalids-MacBook-Air.local>
2025-05-22 15:34:59 +0530

    fix: remove subshell

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ source ./utils/install-progress.sh
 
 if [ "$AUTH_TYPE" = "FIREBASE" ]; then
     if ! [ -f $HOME/firebase-cert.json ]; then
-        echo "firebase cert does not exists. Please paste it in $HOME directory. Name of the file should be \'firebase-cert.json\'"
+        log_error "firebase cert does not exists. Please paste it in $HOME directory. Name of the file should be \'firebase-cert.json\'"
         exit 1
     fi
 fi
