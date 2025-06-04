328fe376186adaa57023c392aaade5f57edc872e
Khalid Khan <khalid@byldd.com>
2025-06-04 19:35:41 +0530

    feat: add env for infisical

--- services/aws-cli.sh
+++ services/aws-cli.sh
@@ -4,6 +4,7 @@ aws_cli() {
     command -v aws
     ;;
   install)
+    sudo apt-get update -y
     sudo apt install -y unzip
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
